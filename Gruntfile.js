c1096a0adf7df018a8fd609db2b8be8d94e2336c
Wisbell <westleyisbell@gmail.com>
2017-01-30 14:18:53 -0600

    Added ability to use jshint for debugging js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,123 @@
+// module.exports = function(grunt) {
+
+//   // Project configuration.
+//   grunt.initConfig({
+//      sass: {
+//         options: {
+//             sourceMap: true,
+//             outputStyle: 'compressed'
+//         },
+//         dist: {
+//             files: {
+//                 'styles/main.css': 'styles/main.scss'
+//             }
+//         }
+//     },
+//     watch: {
+//         files: ['**/*.scss', '<%= jshint.files %>'],
+//         tasks: ['sass', 'jshint']
+//     },
+//    jshint: {
+//       files: ['Gruntfile.js', 'app.js'],
+//       options: {
+//         globals: {
+//           jQuery: true
+//         }
+//       }
+//     },
+//   });
+
+//   // Load the grunt plugins
+//   grunt.loadNpmTasks('grunt-sass');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-contrib-jshint');
+
+
+//   // Default task(s).
+//   grunt.registerTask('default', ['sass', 'jshint', 'watch']);
+
+// };
+
+
+// MY FUCK UP
+
+
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
-     sass: {
+    sass: {
         options: {
             sourceMap: true,
             outputStyle: 'compressed'
         },
         dist: {
             files: {
-                'styles/main.css': 'styles/mains.scss'
+                'styles/main.css': 'styles/main.scss'
             }
         }
     },
-    watch: {
-      sass: {
-        files: ['**/*.scss'],
-        tasks: ['sass']
+    jshint: {
+      files: ['Gruntfile.js', 'app.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
       }
+    },
+    watch: {
+      files: ['**/*.scss', '<%= jshint.files %>'],
+      tasks: ['jshint', 'sass']
     }
   });
 
-  // Load the grunt plugins
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['sass']);
-  grunt.registerTask('westsupersass', ['sass', 'watch']);
+  grunt.registerTask('default', ['sass', 'jshint', 'watch']);
 
 };
 
+
+// THIS WORKS
+
+
+// module.exports = function(grunt) {
+
+//   // Project configuration.
+//   grunt.initConfig({
+//      sass: {
+//         options: {
+//             sourceMap: true,
+//             outputStyle: 'compressed'
+//         },
+//         dist: {
+//             files: {
+//                 'styles/main.css': 'styles/mains.scss'
+//             }
+//         }
+//     },
+//     watch: {
+//       sass: {
+//         files: ['**/*.scss'],
+//         tasks: ['sass']
+//       }
+//     }
+//   });
+
+//   // Load the grunt plugins
+//   grunt.loadNpmTasks('grunt-sass');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+
+//   // Default task(s).
+//   grunt.registerTask('default', ['sass']);
+//   grunt.registerTask('westsupersass', ['sass', 'watch']);
+
+// };
+
+
+
+// MY FAIL BELOW
 
 
 // module.exports = function(grunt) {
---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('app.js loaded');
+
+function stuffs() {
+  console.log('hi');
+  console.log('blah');
+}
+
+stuffs();
